39301268561979e24975ce72bb8e0e9a56e39a70
Kristian Ramirez <Kristian@sids-MacBook-Pro-2.local>
2023-05-03 03:04:47 -0500

    broke up the code into three functions. It currently does not work.

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ https://dev.to/code_mystery/random-password-generator-using-javascript-6a
 https://www.youtube.com/watch?v=x4HUaiazDes
 
 https://developer.mozilla.org/en-US/docs/Web/JavaScript/Data_structures
+
+https://github.com/TiffanyCasey/Password-Generator
--- develop/script.js
+++ develop/script.js
@@ -1,65 +1,65 @@
 // Assignment Code
-//belongs to red button
+//code for red button in the browser
 var generateBtn = document.querySelector("#generate");
 
+var password = "";
 //arrays for password criteria 
-  var password = "";
-  var lowercase = "abcdefghijklmnopqrstuvwxyz";
-  var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var numberic = 0123456789;
-  var specialChars = "!@#$%^&*()";
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialChars = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
 
-  //Declaration variables
-  var length
-  var yesLowercase
-  var yesUppercase
-  var yesNumeric
-  var yesSpecialchars
+//Declaration variables
+var plength
+var yesLowercase
+var yesUppercase
+var yesNumeric
+var yesSpecialchars
+var newLength
+var choices
 
-function generatePassword() {
-  console.log("To generate a new password decide on the following criteria\nClick OK to include character and Cancel to exclude those characters from your password. ")
-  //1. prompt user for password criteria
-  var length = (prompt("Choose a password length between 8 and 128 chracters."));
-  //  a. password length 8 < 128
-    while (length <= 8 || length >= 128) {
-      alert("Password must be bewtween 8 and 128 characters.\nPlease try Again.")
-      var length = (prompt("Choose a password length between 8 and 128 characters."))
-    }
-  //  b. lowercase, uppercase, numbers, special characters
-  var yesLowercase = (confirm("Click OK to include lowercase letters in your password"));
-  var yesUppercase = (confirm("Click OK to include uppercase letters in your password"));
-  var yesNumeric = (confirm("Click OK to include numbers in your password."));
-  var yesSpecialchars = (confirm("Click OK to include special characters in your password"));
+function getPasswordOptions() {
+//1. prompt user for password criteria
+var plength = (prompt("Choose a password length between 8 and 128 chracters."));
+//  a. password length 8 < 128
+if (!plength <= 8 && !plength >= 128) {
+  alert("Password must be bewtween 8 and 128 characters.\nPlease try Again.")
+} else {
+  newLength = alert("Your password will be " + plength + " characters long.");
+}
+//  b. lowercase, uppercase, numbers, special characters
+var yesLowercase = (confirm("Click OK to include lowercase letters in your password"));
+var yesUppercase = (confirm("Click OK to include uppercase letters in your password"));
+var yesNumeric = (confirm("Click OK to include numbers in your password."));
+var yesSpecialchars = (confirm("Click OK to include special characters in your password"));
 
-  var allChoices = [yesLowercase, yesUppercase, yesNumeric, yesSpecialchars]; 
-  //2. Validate the input by 2 criteria
-    while (allChoices === false) {
-      alert("Password must contain at least one character type must be included.")
-      var yesLowercase = (confirm("Click OK to include lowercase letters in your password"));
-      var yesUppercase = (confirm("Click OK to include uppercase letters in your password"));
-      var yesNumeric = (confirm("Click OK to include numbers in your password."));
-      var yesSpecialchars = (confirm("Click OK to include special characters in your password"));
-    } if {
-        var yesLowercase === true(lowercase)
-    }
+var allChoices = [yesLowercase, yesUppercase, yesNumeric, yesSpecialchars];
+//2. Validate the input by 1 criteria
+if (allChoices === false) {
+  alert("Password must contain at least one character type must be included.")
+} 
+}
+
+function getRandom () {
   //3. Generate password
+  var x = Math.random() * 9;
+  console.log(x); 
+} 
+function generatePassword() {
+    //4. Diplay password to the box on the page
+  }
 
+  // Write password to the #password input
+  //create generatePassword function
+  function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+    //displays password on screen
+    passwordText.value = password;
+  }
 
-  //4. Diplay password to the box on the page
-  return "generated password will go here"
-}
-
-// Write password to the #password input
-//create generatePassword function
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-//displays password on screen
-  passwordText.value = password;
-}
-
-// Add event listener to generate button
-//calls function
-generateBtn.addEventListener("click", writePassword);
+  // Add event listener to generate button
+  //calls function
+  generateBtn.addEventListener("click", writePassword);
 
 
